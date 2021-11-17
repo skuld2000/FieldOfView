b975a2e523b80c599572b60a1426de6e1eed2c8e
skuld2000 <skuld2000@linecorp.com>
2021-11-17 23:00:12 +0900

    Add a FieldOfView Project.

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.DrawLine(Vector3.one * 5f, Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
